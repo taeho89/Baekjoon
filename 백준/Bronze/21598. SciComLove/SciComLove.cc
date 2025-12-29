4d66c07c64a29f474244fe1de847933a102cf329
Taeho <164999649+taeho89@users.noreply.github.com>
2025-12-30 00:18:51 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace    std;
+
+int    main(void) {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    int    N;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cout << "SciComLove" << '\n';
+    }
+}
