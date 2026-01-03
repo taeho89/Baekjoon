8d12c7743deaada86f5dec4ffb6259687f5b97f0
Taeho <164999649+taeho89@users.noreply.github.com>
2026-01-03 16:59:50 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace    std;
+
+int    main(void) {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    int a, b;
+    cin >> b;
+    a = b - b / 11;
+    cout << a;
+}
